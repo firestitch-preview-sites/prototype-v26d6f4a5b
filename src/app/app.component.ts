701299c42976b7b238f6c2cae3b3532ad805c902
raygig <ray@firestitch.com>
2026-01-28 13:08:35 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: `<h1>Preview Site</h1>`,
+  template: `<h1>Preview Site!!!!</h1>`,
   styles: [`h1 { margin: 20px; }`],
 })
 export class AppComponent {
